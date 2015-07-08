6101cf4c319adaba20c29540016658eece04bdcd
Irakli Nadareishvili <irakli@gmail.com>
2015-07-08 05:46:32 +0000

    improved hints. fixed incorrect levelindex counting in solution modules

--- securitygame.js
+++ securitygame.js
@@ -27,7 +27,7 @@ function setupChallengeLevel(currLevel, levelIndex, allLevels) {
   // Level's task
   
   // Humans love indexes that start with 1, but machins love 0s
-  currLevel.index = levelIndex + 1;
+  currLevel.index = levelIndex;
   module.exports.namedIndex[currLevel.name] = currLevel;
     
   var props = {};
